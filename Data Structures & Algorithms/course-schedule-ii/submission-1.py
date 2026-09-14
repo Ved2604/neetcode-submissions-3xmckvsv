b756f752a3ea2176ebf88d52af9ca483c9eb82fb
Ved2604 <59091710+Ved2604@users.noreply.github.com>
2026-09-15 05:11:17 +0530

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.py
@@ -0,0 +1,46 @@
+from collections import deque
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        preqmap={x:[] for x in range(numCourses)}
+        for u,v in prerequisites:
+            preqmap[u].append(v)
+        
+
+        visited=set()
+        output=[]
+
+        def dfs_is_cycle(start,path=None):  # cycle detection for directed graph
+            
+            visited.add(start)
+            if not path:
+                path={start}
+            else: 
+                path.add(start)    
+            for nei in preqmap[start]:
+                if nei in path:
+                    return True
+                elif nei not in visited:                    
+                    if dfs_is_cycle(nei,path):
+                        return True
+            path.remove(start)        
+            output.append(start)
+            return False        
+
+
+
+        for node in range(numCourses):
+            if node not in visited:
+                if dfs_is_cycle(node):
+                    return []
+
+        return output            
+        
+        
+
+
+
+
+        
+
+
+
